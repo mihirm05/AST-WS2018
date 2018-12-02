aed05ed6b89ff0541d2e675c0c80f418488e790e
Mihir <mihirmulye95@gmail.com>
2018-12-02 21:27:33 +0100

    Adding the SensorData.java file

---
+++ src/SensorData.java
@@ -0,0 +1,28 @@
+package ast.objectListMerger;
+public class SensorData{
+
+	String itemType; 
+	int itemLabel;
+	double percentageSurety;
+	
+	public SensorData(String objectType, int objectLabel, double percentageSurety) {
+		this.itemType = objectType;
+		this.itemLabel = objectLabel;
+		this.percentageSurety = percentageSurety;
+	}
+	
+	@Override
+	public String toString() {
+		return "(" +this.itemType+ "," +this.itemLabel+ "," +this.percentageSurety+ ")" ;
+	}
+	
+	public SensorData comparePercentageSurety(SensorData sensorData1) {		
+		if (this.percentageSurety > sensorData1.percentageSurety) {
+			return this;
+		}
+		
+		else {
+			return sensorData1;
+		}
+	}
+}
