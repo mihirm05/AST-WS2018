df0e1c73aad2f2dad16aaf987eb28e7b281a2b6d
Mihir <mihirmulye95@gmail.com>
2018-12-09 09:39:25 +0100

    Adding Notes for the code and test cases

--- src/Main.java
+++ src/Main.java
@@ -6,46 +6,80 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//sensor 1 algorithm 1
 		SensorData rgbd1 = new SensorData("knife", 1, 0.99);
 		SensorData rgbd2 = new SensorData("scissor", 2, 0.65);
 		SensorData rgbd3 = new SensorData("spoon", 3, 0.33);
 		SensorData rgbd4 = new SensorData("spoon", 4, 0.80);
 		SensorData rgbd5 = new SensorData("keys", 5, 0.95);
 		
-		ArrayList<SensorData> sensor1 = new ArrayList<SensorData>(20);
-		sensor1.add(rgbd1);
-		sensor1.add(rgbd2);
-		sensor1.add(rgbd3);
-		sensor1.add(rgbd4);
-		sensor1.add(rgbd5);
-//		System.out.println(sensor1);
+		ArrayList<SensorData> sensor1Algorithm1 = new ArrayList<SensorData>(20);
+		sensor1Algorithm1.add(rgbd1);
+		sensor1Algorithm1.add(rgbd2);
+		sensor1Algorithm1.add(rgbd3);
+		sensor1Algorithm1.add(rgbd4);
+		sensor1Algorithm1.add(rgbd5);
 		
+		//sensor 2 algorithm 1 
 		SensorData rgb1 = new SensorData("knife", 1, 0.55);
 		SensorData rgb2 = new SensorData("scissors", 2, 0.95);
 		SensorData rgb3 = new SensorData("fork", 3, 0.99);
 		SensorData rgb4 = new SensorData("spoon", 4, 0.99);
 		SensorData rgb5 = new SensorData("keys", 5, 0.95);
 		
-		ArrayList<SensorData> sensor2 = new ArrayList<SensorData>(20);
-		sensor2.add(rgb1);
-		sensor2.add(rgb2);
-		sensor2.add(rgb3);
-		sensor2.add(rgb4);
-		sensor2.add(rgb5);
+		ArrayList<SensorData> sensor2Algorithm1 = new ArrayList<SensorData>(20);
+		sensor2Algorithm1.add(rgb1);
+		sensor2Algorithm1.add(rgb2);
+		sensor2Algorithm1.add(rgb3);
+		sensor2Algorithm1.add(rgb4);
+		sensor2Algorithm1.add(rgb5);
+		
+		//sensor 1 algorithm 2
+		SensorData rgbd12 = new SensorData("knife", 1, 0.89);
+		SensorData rgbd22 = new SensorData("scissor", 2, 0.55);
+		SensorData rgbd32 = new SensorData("spoon", 3, 0.63);
+		SensorData rgbd42 = new SensorData("spoon", 4, 0.70);
+		SensorData rgbd52 = new SensorData("keys", 5, 0.95);
+		
+		ArrayList<SensorData> sensor1Algorithm2 = new ArrayList<SensorData>(20);
+		sensor1Algorithm2.add(rgbd1);
+		sensor1Algorithm2.add(rgbd2);
+		sensor1Algorithm2.add(rgbd3);
+		sensor1Algorithm2.add(rgbd4);
+		sensor1Algorithm2.add(rgbd5);
+		
+		//sensor 2 algorithm 2 
+		SensorData rgb12 = new SensorData("knife", 1, 0.91);
+		SensorData rgb22 = new SensorData("scissor", 2, 0.60);
+		SensorData rgb32 = new SensorData("spoon", 3, 0.30);
+		SensorData rgb42 = new SensorData("spoon", 4, 0.81);
+		SensorData rgb52 = new SensorData("keys", 5, 0.96);
+		
+		ArrayList<SensorData> sensor2Algorithm2 = new ArrayList<SensorData>(20);
+		sensor2Algorithm2.add(rgbd1);
+		sensor2Algorithm2.add(rgbd2);
+		sensor2Algorithm2.add(rgbd3);
+		sensor2Algorithm2.add(rgbd4);
+		sensor2Algorithm2.add(rgbd5);
 		
 		ArrayList<SensorData> decision = new ArrayList<SensorData>(20);
 		
-		for(int i=0; i<sensor1.size();i++){
-			decision.add(sensor1.get(i).comparePercentageSurety(sensor2.get(i)));
+		
+		if ((sensor1Algorithm2.size() == sensor2Algorithm2.size()) && rgb1.itemType != " "){
+			for(int i=0; i<sensor1Algorithm2.size();i++){
+				decision.add(sensor1Algorithm2.get(i).comparePercentageSurety(sensor2Algorithm2.get(i),sensor1Algorithm1.get(i),sensor2Algorithm1.get(i)));
+			}
 		}
+		
+		else {
+			System.out.println("Insufficient Data");
+			}
 		System.out.println(decision);
-		SensorData laser1 = new SensorData("knife", 1, 0.50);
-		SensorData laser2 = new SensorData("scissors", 2, 0.5);
-		SensorData laser3 = new SensorData("spoon", 3, 0.90);
-		SensorData laser4 = new SensorData("spoon", 4, 0.90);
-		SensorData laser5 = new SensorData("keys", 5, 0.99);				
 	}
 }
+
+
+
 	
 
 
--- src/SensorTest.java
+++ src/SensorTest.java
@@ -2,20 +2,57 @@ package ast.objectListMerger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class SensorTest {
 
-
 	@Test
 	void testCompareObjects() {
+		//testcase to handle the standard data from sensors
 		SensorData rgbd1 = new SensorData("knife", 1, 0.99);
-		SensorData rgb1 = new SensorData("knife", 1, 0.55);
-		SensorData rgb2 = new SensorData("knife", 1, 0.99);
-		Assert.assertEquals(rgbd1, rgbd1.comparePercentageSurety(rgb1));	
-		Assert.assertEquals(rgb2, rgbd1.comparePercentageSurety(rgb2));
+		SensorData rgb1 = new SensorData("knife", 1, 0.95);
+		SensorData rgbd11 = new SensorData("knife", 1 , 0.90);
+		SensorData rgb11 = new SensorData("knife", 1, 0.50);
+		Assert.assertEquals(rgbd1, rgbd1.comparePercentageSurety(rgb1,rgbd11,rgb11));	
+		System.out.println("Test 1 passed");
 		
+		//testcase to handle data different itemLabel
+		SensorData rgbd2 = new SensorData("scissor", 2, 0.65);
+		SensorData rgb2 = new SensorData("spoon", 4, 0.99);
+		SensorData rgbd22 = new SensorData("spoon", 2 , 0.51);
+		SensorData rgb22 = new SensorData("spoon",2,0.50);
+		SensorData temporaryObject = new SensorData(" This is a garbage Value ", 0, 0);
+		temporaryObject.itemType.equals(rgbd2.comparePercentageSurety(rgb2,rgbd22,rgb22));
+		System.out.println("Test 2 passed");
+		
+		//testcase to handle anomalous itemType classification 
+		SensorData rgbd3 = new SensorData("spoon", 3, 0.33);
+		SensorData rgb3 = new SensorData("fork", 3, 0.99);
+		SensorData rgbd33 = new SensorData("knife", 3, 0.99);
+		SensorData rgb33 = new SensorData("spoon", 3, 0.92);
+		Assert.assertEquals(rgb3, rgbd3.comparePercentageSurety(rgb3,rgbd33,rgb33));
+		System.out.println("Test 3 passed");
+		
+		//testcase to handle empty list of objects 
+		SensorData rgbd4 = new SensorData(" ", 0, 0);
+		SensorData rgb4 = new SensorData(" ",0,0);
+		SensorData rgbd44 = new SensorData(" ",0,0);
+		SensorData rgb44 = new SensorData(" ",0,0);
+//		Assert.assertEquals(rgb4, rgbd4.comparePercentageSurety(rgb4));
+		"Insufficient Data".equals(rgb4.comparePercentageSurety(rgbd4,rgbd44,rgb44));
+		System.out.println("Test 4 passed");
+		
+		//testcase to handle different case of itemType
+		SensorData rgbd5 = new SensorData("spoon", 3, 0.33);
+		SensorData rgb5 = new SensorData("SPOON", 3, 0.33);
+		SensorData rgbd55 = new SensorData("spoon", 3, 0.33);
+		SensorData rgb55 = new SensorData("SPOON", 3, 0.33);
+		Assert.assertEquals(rgbd5, rgb5.comparePercentageSurety(rgbd5,rgbd55,rgb55));
+		System.out.println("Test 5 passed");
 	}
-
 }
+
+
